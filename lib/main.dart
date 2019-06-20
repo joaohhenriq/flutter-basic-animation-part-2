ff2961c3d3f2bbed81043554540c2e3d4867ae21
joaoh <joao_camargo10@hotmail.com>
2019-06-19 21:20:14 -0300

    Realizando a animação do container

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,56 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation animation;
+
+  void mylistener(status){
+    if (status == AnimationStatus.completed) {
+      animation.removeStatusListener(mylistener);
+      animationController.reset();
+      animation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+          parent: animationController, curve: Curves.fastOutSlowIn));
+
+      animationController.forward();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController, curve: Curves.fastOutSlowIn))
+      ..addStatusListener(mylistener);
+
+    animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation 2"),
+        centerTitle: true,
+      ),
+      body: AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) => Transform(
+                transform: Matrix4.translationValues(
+                    animation.value * width, 0.0, 0.0),
+                child: Center(
+                  child: Container(
+                    height: 200.0,
+                    width: 200.0,
+                    color: Colors.green,
+                  ),
+                ),
+              )),
+    );
   }
 }
-
